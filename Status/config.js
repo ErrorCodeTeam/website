f19e1720fad76781616f65353ebfd83f507ca55e
baozishu <1771575735@qq.com>
2024-08-09 18:41:36 +0800

    更新 Status

---
+++ Status/config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Public Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u886978-f722aea9de145ef225d7a14a',
+  ],
+
+  // 日志天数
+  CountDays: 30,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Error Code Team',
+      url: 'https://353.ee/'
+    },
+    {
+      text: '吐槽一下',
+      url: 'https://txc.qq.com/products/39844'
+    },
+    {
+      text: 'Blog',
+      url: 'https://ono.ee/'
+    },
+  ],
+};
